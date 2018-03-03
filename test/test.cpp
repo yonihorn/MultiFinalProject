56099b4172d77e72ad2f301d18800e4b69df35a4
udiz <udirot1@gmail.com>
2018-03-03 18:18:08 +0200

    add test for CAS

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <atomic>
+#include <cassert>
 #include <immintrin.h>
 
+
+int test_cas() {
+    std::atomic<unsigned int> variable;
+    unsigned int old_true = 1;
+    unsigned int old_false = 3;
+    unsigned int new_ = 2;
+    variable.store(1);
+    std::atomic_compare_exchange_strong<unsigned int>(&variable, &old_false, 2);
+    assert(variable.load(std::memory_order_relaxed) == 1);
+    std::atomic_compare_exchange_strong<unsigned int>(&variable, &old_true, 2);
+    std::cout << variable.load(std::memory_order_relaxed) << std::endl;
+    assert(variable.load(std::memory_order_relaxed) == 2);
+
+}
+
 int main(int argc, char** argv) {
+    test_cas();
     unsigned status;
     if ((status = _xbegin() == _XBEGIN_STARTED)) {
         std::cout << "A" << std::endl;
@@ -31,3 +49,4 @@ int main(int argc, char** argv) {
         std::cout << "Fallback2 - got " << status2 << std::endl;
     }
 }
+
