ea50090a80396d34f5dce21089a366b57a224e66
udiz <udirot1@gmail.com>
2018-03-10 21:05:29 +0200

    add u_int specialization to HTM_CAS

--- htm_cas.h
+++ htm_cas.h
@@ -2,17 +2,34 @@
 #include <cstring>
 
 template <class T> 
-bool htm_compare_and_swap(T* obj, T* expected, T desired) {
+inline bool htm_compare_and_swap(T* obj, T* expected, T desired) {
     unsigned status;
-    if (status = _xbegin() == _XBEGIN_STARTED) {
-        if (std::memcmp(obj, expected, sizeof(T)) == 0) {
-            std::memcpy(obj, &desired, sizeof(T));
-            _xend();
-            return true;
-        } else {
-            _xend();
+    if (std::memcmp(obj, expected, sizeof(T)) == 0) {
+        if (status = _xbegin() == _XBEGIN_STARTED) {
+            if (std::memcmp(obj, expected, sizeof(T)) == 0) {
+                std::memcpy(obj, &desired, sizeof(T));
+                _xend();
+                return true;
+            } else {
+                _xend();
+            }
         }
     }
-    
     return false;
+}
+
+template<>
+inline bool htm_compare_and_swap<u_int>(u_int* obj, u_int* expected, u_int desired) {
+    unsigned status;
+    if (*obj == *expected) {
+        if (status = _xbegin() == _XBEGIN_STARTED) {
+            if (*obj == *expected) {
+                *obj = desired;
+                _xend();
+                return true;
+            } else {
+                _xend();
+            }
+        }
+    }
 }
