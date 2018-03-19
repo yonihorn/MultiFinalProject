ce83fd73a467c4bd9df0efd50fd44c88ff1ef500
noakor <noakor1@gmail.com>
2018-03-19 19:53:19 +0200

    read object just after transaction start

--- htm_cas.h
+++ htm_cas.h
@@ -3,17 +3,16 @@
 
 template <class T> 
 inline bool htm_compare_and_swap(T* obj, T* expected, T desired) {
-    unsigned status;
-    if (std::memcmp(obj, expected, sizeof(T)) == 0) {
-        if (status = _xbegin() == _XBEGIN_STARTED) {
-            if (std::memcmp(obj, expected, sizeof(T)) == 0) {
-                std::memcpy(obj, &desired, sizeof(T));
-                _xend();
-                return true;
-            } else {
-                _xend();
-            }
+    unsigned status;    
+    if (status = _xbegin() == _XBEGIN_STARTED) {
+        if (std::memcmp(obj, expected, sizeof(T)) == 0) {
+            std::memcpy(obj, &desired, sizeof(T));
+            _xend();
+            return true;
+        } else {
+            _xend();
         }
+        
     }
     return false;
 }
@@ -21,15 +20,13 @@ inline bool htm_compare_and_swap(T* obj, T* expected, T desired) {
 template<>
 inline bool htm_compare_and_swap<u_int>(u_int* obj, u_int* expected, u_int desired) {
     unsigned status;
-    if (*obj == *expected) {
-        if (status = _xbegin() == _XBEGIN_STARTED) {
-            if (*obj == *expected) {
-                *obj = desired;
-                _xend();
-                return true;
-            } else {
-                _xend();
-            }
+    if (status = _xbegin() == _XBEGIN_STARTED) {
+        if (*obj == *expected) {
+            *obj = desired;
+            _xend();
+            return true;
+        } else {
+            _xend();
         }
     }
     return false;
