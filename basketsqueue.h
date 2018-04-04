23a722364e3c8fa2dbf2d347f25b4f7708820e74
Yoni Hornstein <yoni.horn@gmail.com>
2018-04-05 00:47:21 +0300

    modifications to pointer_t, and defining atomic_pointer_t

--- basketsqueue.h
+++ basketsqueue.h
@@ -1,15 +1,20 @@
+#pragma once
+
 #include <atomic>
 
 template <typename T>
 struct node_t;
 
 template <typename T>
-struct pointer_t
+class pointer_t
 {
-    pointer_t(node_t<T>* p_node = nullptr, bool deleted = false, unsigned int tag = 0 ) : 
-    pointer(p_node),
-    deleted(deleted),
-    tag(tag) {}
+public:
+	pointer_t() = default;
+
+	pointer_t(node_t<T>* p_node, bool deleted = false, unsigned int tag = 0) :
+		pointer(p_node),
+		deleted(deleted),
+		tag(tag) {};
 
     node_t<T>* pointer;
     bool deleted;
@@ -17,11 +22,18 @@ struct pointer_t
 };
 
 template <typename T>
+using atomic_pointer_t = std::atomic<pointer_t<T>>;
+
+template <typename T>
 struct node_t
 {
-    T value;
-    pointer_t<T> next;
+	node_t() {};
+	node_t(T value) : value(value) {};
+	T value;
+    atomic_pointer_t<T> next;
 };
+
+
 
 template <typename T>
 class BasketsQueue
@@ -30,12 +42,51 @@ class BasketsQueue
         BasketsQueue()
         {
             auto node = new node_t<T>();
-            tail = pointer_t<T>(node);
-            head = pointer_t<T>(node);
-		};
+            b_tail = pointer_t<T>(node);
+            b_head = pointer_t<T>(node);
+		}
+
+		bool Enqueue(T value)
+		{
+			node_t<T>* new_node = new node_t<T>(value);
+			
+			while (true)
+			{
+				pointer_t<T> tail = this->b_tail;
+				pointer_t<T> next = tail.pointer->next;
+
+				if (tail == b_tail)
+				{
+					if (nullptr == next.pointer)
+					{
+						new_node->next = pointer_t<T>(nullptr, 0, tail.tag + 2);
+						if (std::atomic_compare_exchange_strong(&tail.pointer->next, &next, pointer_t<T>(new_node, 0, tail.tag + 1)))
+						{
+							std::atomic_compare_exchange_strong(&b_tail, &tail, pointer_t<T>(new_node, 0, tail.tag + 1));
+							return true;
+						}
+						next = tail.pointer->next;
+						while ((next.tag == tail.tag + 1) && (false == next.deleted))
+						{
+							// backoff_scheme() ??? 
+							new_node->next = next;
+							if (std::atomic_compare_exchange_strong(&tail.pointer->next, &next, pointer_t<T>(new_node, 0, tail.tag + 1)))
+								return true;
+							next = tail.pointer->next;
+						}
+					}
+					else
+					{
+						while ((nullptr != next.pointer->next.pointer) && (tail == b_tail))
+							next = next.pointer->next;
+						std::atomic_compare_exchange_strong(&b_tail, &tail, poiner_t<T>(next.pointer, 0, tail.tag + 1));
+					}
+				}
+			}
+		}
     
     private:
-		pointer_t<T> tail;
-		pointer_t<T> head;
-    
+		atomic_pointer_t<T> b_tail;
+		atomic_pointer_t<T> b_head;
 };
+
