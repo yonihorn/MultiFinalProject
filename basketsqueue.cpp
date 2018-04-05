5b50e6e221c4c00ec82dc49d3d0eca4919c30a35
noakor <noakor1@gmail.com>
2018-04-05 23:57:08 +0300

    dequeue-works-without-concurrency

--- basketsqueue.cpp
+++ basketsqueue.cpp
@@ -3,8 +3,21 @@
 int main()
 {
 	BasketsQueue<int> queue;
+	std::cout << "the value is: " << queue.dequeue() << std::endl;
 	queue.Enqueue(1);
 	queue.Enqueue(2);
+	queue.Enqueue(3);
+	queue.Enqueue(4);
+	queue.Enqueue(5);
+	std::cout << "the value is: " << queue.dequeue() << std::endl;
+	std::cout << "the value is: " << queue.dequeue() << std::endl;
+	std::cout << "the value is: " << queue.dequeue() << std::endl;
+	std::cout << "the value is: " << queue.dequeue() << std::endl;
+	std::cout << "the value is: " << queue.dequeue() << std::endl;
+	queue.Enqueue(6);
+	queue.Enqueue(7);
+	std::cout << "the value is: " << queue.dequeue() << std::endl;
+	std::cout << "the value is: " << queue.dequeue() << std::endl;
 
 	return 1;
 }
--- basketsqueue.h
+++ basketsqueue.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <atomic>
+#include <iostream>
+#define MAX_HOPS (3)
 
 template <typename T>
 struct node_t;
@@ -9,17 +11,17 @@ template <typename T>
 class pointer_t
 {
 public:
-	pointer_t() = default;
+    pointer_t() = default;
 
-	pointer_t(node_t<T>* p_node, bool deleted = false, unsigned int tag = 0) :
-		pointer(p_node),
-		deleted(deleted),
-		tag(tag) {};
+    pointer_t(node_t<T>* p_node, bool deleted = false, unsigned int tag = 0) :
+        pointer(p_node),
+        deleted(deleted),
+        tag(tag) {};
 
-	bool operator==(const pointer_t& other) const
-	{
-		return pointer == other.pointer;
-	}
+    bool operator==(const pointer_t& other) const
+    {
+        return pointer == other.pointer;
+    }
     node_t<T>* pointer = nullptr;
     bool deleted = false;
     unsigned int tag = 0;
@@ -31,9 +33,9 @@ using atomic_pointer_t = std::atomic<pointer_t<T>>;
 template <typename T>
 struct node_t
 {
-	node_t() {};
-	node_t(T value) : value(value) {};
-	T value;
+    node_t() {};
+    node_t(T value) : value(value) {};
+    T value;
     atomic_pointer_t<T> next;
 };
 
@@ -42,55 +44,142 @@ struct node_t
 template <typename T>
 class BasketsQueue
 {
-    public:
-        BasketsQueue()
+public:
+    BasketsQueue()
+    {
+        auto node = new node_t<T>();
+        b_tail = pointer_t<T>(node);
+        b_head = pointer_t<T>(node);
+    }
+
+
+    void free_chain(pointer_t<T>& head, pointer_t<T> new_head)
+    {
+        if (std::atomic_compare_exchange_strong(&b_head, &head, pointer_t<T>(new_head.pointer, 0, head.tag + 1)))
         {
-            auto node = new node_t<T>();
-            b_tail = pointer_t<T>(node);
-            b_head = pointer_t<T>(node);
-		}
+            while (!(head.pointer == new_head.pointer))
+            {
+                pointer_t<T> next = head.pointer->next;
+                delete head.pointer;
+                head = next;
+                
+            }
+        }
+    }
 
-		bool Enqueue(T value)
-		{
-			node_t<T>* new_node = new node_t<T>(value);
-			
-			while (true)
-			{
-				pointer_t<T>& tail = this->b_tail.load();
-				pointer_t<T>& next = tail.pointer->next.load();
+    T dequeue()
+    {
+        while (true)
+        {
+            pointer_t<T>& head = b_head.load();
+            pointer_t<T>& tail = b_tail.load();
+            pointer_t<T>& next = head.pointer->next.load();
+            if (tail == b_tail.load())
+            {
+                if (head.pointer == tail.pointer)
+                {
+                    if (next.pointer == NULL)
+                    {
+                        std::cout << "empty!" << std::endl;
+                        return NULL;
+                    }
+                    while (next.pointer->next.load().pointer != NULL && b_tail.load() == tail)
+                    {
+                        next = next.pointer->next;
+                    }
+                    std::atomic_compare_exchange_strong(&b_tail, &tail,
+                        pointer_t<T>(next.pointer, 0, tail.tag + 1));
+                }
 
-				if (tail == b_tail)
-				{
-					if (nullptr == next.pointer)
-					{
-						new_node->next = pointer_t<T>(nullptr, 0, tail.tag + 2);
-						if (tail.pointer->next.compare_exchange_strong(next, pointer_t<T>(new_node, false, tail.tag + 1)))
-						{
-							b_tail.compare_exchange_strong(tail, pointer_t<T>(new_node, false, tail.tag + 1));
-							return true;
-						}
-						next = tail.pointer->next;
-						while ((next.tag == tail.tag + 1) && (false == next.deleted))
-						{
-							// backoff_scheme() ??? 
-							new_node->next = next;
-							if (tail.pointer->next.compare_exchange_strong(next, pointer_t<T>(new_node, false, tail.tag + 1)))
-								return true;
-							next = tail.pointer->next.load();
-						}
-					}
-					else
-					{
-						while ((nullptr != next.pointer->next.load().pointer) && (tail == b_tail))
-							next = next.pointer->next;
-						b_tail.compare_exchange_strong(tail, pointer_t<T>(next.pointer, false, tail.tag + 1));
-					}
-				}
-			}
-		}
-    
-    private:
-		atomic_pointer_t<T> b_tail;
-		atomic_pointer_t<T> b_head;
+                else
+                {
+                    int hops = 0;
+                    pointer_t<T> iter = b_head.load();
+                    while (next.deleted && iter.pointer != tail.pointer && b_head.load() == head)
+                    {
+                        iter = next;
+                        next = iter.pointer->next;
+                        hops++;
+                    }
+                    if (!(b_head.load() == head))
+                    {
+                        continue;
+                    }
+                    else if (iter.pointer == tail.pointer)
+                    {
+                        free_chain(head, iter);
+                    }
+                    else
+                    {
+                        T value = next.pointer->value;
+                        if (std::atomic_compare_exchange_strong(&iter.pointer->next, &next,
+                            pointer_t<T>(next.pointer, 1, tail.tag + 1)))
+                        {
+                            if (hops >= MAX_HOPS)
+                            {
+                                free_chain(head, next);
+                            }
+
+                            return value;
+                        }
+                        // backoff scheme
+                    }
+
+                }
+            }
+        }
+    }
+
+    int sum()
+    {
+        pointer_t<T> iter = b_head.load();
+        while (next.deleted && iter.pointer != tail.pointer && b_head.load() == head)
+        {
+            iter = next;
+            next = iter.pointer->next;
+            hops++;
+        }
+    }
+    bool Enqueue(T value)
+    {
+        node_t<T>* new_node = new node_t<T>(value);
+
+        while (true)
+        {
+            pointer_t<T>& tail = this->b_tail.load();
+            pointer_t<T>& next = tail.pointer->next.load();
+
+            if (tail == b_tail)
+            {
+                if (nullptr == next.pointer)
+                {
+                    new_node->next = pointer_t<T>(nullptr, 0, tail.tag + 2);
+                    if (tail.pointer->next.compare_exchange_strong(next, pointer_t<T>(new_node, false, tail.tag + 1)))
+                    {
+                        b_tail.compare_exchange_strong(tail, pointer_t<T>(new_node, false, tail.tag + 1));
+                        return true;
+                    }
+                    next = tail.pointer->next;
+                    while ((next.tag == tail.tag + 1) && (false == next.deleted))
+                    {
+                        // backoff_scheme() ??? 
+                        new_node->next = next;
+                        if (tail.pointer->next.compare_exchange_strong(next, pointer_t<T>(new_node, false, tail.tag + 1)))
+                            return true;
+                        next = tail.pointer->next.load();
+                    }
+                }
+                else
+                {
+                    while ((nullptr != next.pointer->next.load().pointer) && (tail == b_tail))
+                        next = next.pointer->next;
+                    b_tail.compare_exchange_strong(tail, pointer_t<T>(next.pointer, false, tail.tag + 1));
+                }
+            }
+        }
+    }
+
+private:
+    atomic_pointer_t<T> b_tail;
+    atomic_pointer_t<T> b_head;
 };
-
