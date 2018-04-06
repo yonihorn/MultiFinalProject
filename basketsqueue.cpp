3eae843e28a8d5daa36aa60267c85c31cb19cdce
noakor <noakor1@gmail.com>
2018-04-06 19:22:32 +0300

    add new test

--- basketsqueue.cpp
+++ basketsqueue.cpp
@@ -4,11 +4,20 @@
 
 #define VERIFY_ELSE_RETURN(expected, actual, variable_name) if (actual != expected) { std::cout << variable_name << " should be " << expected << " but instead equals to " << actual << std::endl; return 1; }
 
-void thread_callback(BasketsQueue<int>* queue, int number_of_enqueues)
+void enqueue_callback(BasketsQueue<int>* queue, int number_of_enqueues)
 {
 	for (int i = 0; i < number_of_enqueues; i++)
 	{
 		queue->enqueue(i);
+	}
+}
+
+void enqueue_dequeue_callback(BasketsQueue<int>* queue, int number_of_enqueues)
+{
+	for (int i = 0; i < number_of_enqueues; i++)
+	{
+		queue->enqueue(i);
+		queue->dequeue();
 	}
 }
 
@@ -44,7 +53,7 @@ int basic_enqueue_dequeue_test()
 
 	return 0;
 }
-int async_test()
+int async_test_enqueue()
 {
 	BasketsQueue<int> queue;
 
@@ -54,7 +63,7 @@ int async_test()
 	std::vector<std::future<void>> futures;
 	for (int i = 0; i < number_of_threads; i++)
 	{
-		futures.emplace_back(std::async(thread_callback, &queue, number_of_enqueues_per_thread));
+		futures.emplace_back(std::async(enqueue_callback, &queue, number_of_enqueues_per_thread));
 	}
 
 	for (int i = 0; i < futures.size(); i++)
@@ -67,15 +76,40 @@ int async_test()
 
 	return 0;
 }
+
+int async_test_enqueue_dequeue()
+{
+	BasketsQueue<int> queue;
+
+	int number_of_threads = 4;
+	int number_of_enqueues_per_thread = 50;
+
+	std::vector<std::future<void>> futures;
+	for (int i = 0; i < number_of_threads; i++)
+	{
+		futures.emplace_back(std::async(enqueue_dequeue_callback, &queue, number_of_enqueues_per_thread));
+	}
+
+	for (int i = 0; i < futures.size(); i++)
+	{
+		futures[i].wait();
+	}
+	futures.clear();
+
+	VERIFY_ELSE_RETURN(0, queue.size(), "size");
+
+	return 0;
+}
+
 int main()
 {
 	if (0 != basic_enqueue_dequeue_test() ||
-		0 != async_test())
+		0 != async_test_enqueue() || 0 != async_test_enqueue_dequeue())
 	{
 		std::cout << "Some tests failed " << std::endl;
 		return 1;
 	}
 	std::cout << "Tests succeeded" << std::endl;
 	return 0;
-	
+
 }
